cd809439a487b6f64c8d79655520f620944c1ca6
ira <nirajktwl@gmail.com>
2010-05-30 01:48:13 +0545

    base_title helper added

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
   integrate_views
+  
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -10,8 +14,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'home'
-      response.should have_tag("title",
-                              "Ruby on Rails Tutorial Sample App | Home")
+      response.should have_tag("title", @base_title + " | Home")
     end
   end
 
@@ -22,7 +25,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_tag("title", "Ruby on Rails Tutorial Sample App | Contact")
+      response.should have_tag("title", @base_title + " | Contact")
     end  
   end
   describe "GET 'about'" do
@@ -32,7 +35,7 @@ describe PagesController do
       end
     it "should have the right title" do
       get 'about'
-      response.should have_tag("title", "Ruby on Rails Tutorial Sample App | About")
+      response.should have_tag("title", @base_title + " | About")
     end  
 
     end
